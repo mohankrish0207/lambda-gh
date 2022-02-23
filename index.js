ff3ef7451cc74834a983621460e42721d726afa3
mohankrish0207 <86762895+mohankrish0207@users.noreply.github.com>
2022-02-23 14:24:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
           const response = {
                       statusCode: 200,
-                      body: JSON.stringify("Hello from mohan"),
+                      body: JSON.stringify("Hello from github"),
                     }
           return response
 }
